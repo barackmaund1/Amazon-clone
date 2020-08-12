45627170fa6fab30c3d402aed8f6d746704895a7
barackmaund1 <pmaundu49@gmail.co>
2020-08-12 09:01:09 +0300

    setting firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDTgPrLAbZsb43we6cGMqkuumU_1_ZJUY",
+    authDomain: "clone-a2dde.firebaseapp.com",
+    databaseURL: "https://clone-a2dde.firebaseio.com",
+    projectId: "clone-a2dde",
+    storageBucket: "clone-a2dde.appspot.com",
+    messagingSenderId: "625106612142",
+    appId: "1:625106612142:web:6ddc842ed5aac4007a223f",
+    measurementId: "G-FXGNF7W6HE"
+  };
+
+  
