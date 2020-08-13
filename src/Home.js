dfa7277cb2f6c975e2715878f8e4ae3f02c91bf4
barackmaund1 <pmaundu49@gmail.co>
2020-08-13 09:44:33 +0300

    build product component

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 const Home = () => {
     return (
@@ -8,7 +9,60 @@ const Home = () => {
           className='home__image' 
           src='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg'/>
           
-          </div>
+          {/* Product id,title,price,rating,image*/}
+           {/*product*/}
+           <div className='home__row'>
+                <Product
+                id='12345'
+                title='Fire TV Stick streaming media '
+                price='120.50'
+                rating={5}
+                image='https://images-na.ssl-images-amazon.com/images/I/61juvPBQCsL._AC_SL1000_.jpg'
+                />
+                <Product
+                id='12345'
+                title='Apple iPad '
+                price='120.50'
+                rating={5}
+                image='https://images-na.ssl-images-amazon.com/images/I/41h0H4SCgPL._AC_US160_.jpg'
+                />
+               
+           </div>
+           <div className='home__row'>
+           <Product
+                id='12345'
+                title='Computer Accessories '
+                price='120.50'
+                rating={5}
+                image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-244337D24F250BSI._V533746475_.jpg'
+                />
+                <Product
+                id='12345'
+                title='Computer componenent '
+                price='120.50'
+                rating={5}
+                image='https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2447E617EC5C463L._V533746472_.jpg'
+                />
+                <Product
+                id='12345'
+                title='Fire TV Stick streaming media '
+                price='120.50'
+                rating={5}
+                image='https://images-na.ssl-images-amazon.com/images/I/71gjJZ4Df2L._AC_UX679_.jpg'
+                />
+               
+           </div>
+           <div className='home__row'>
+                <Product
+                id='12345'
+                title='Fire TV Stick streaming media '
+                price='120.50'
+                rating={5}
+                image='https://images-na.ssl-images-amazon.com/images/I/51I3UjD-Q1L._AC_US160_.jpg'
+                />
+           </div>
+          
+        </div>
     )
 }
 
---
+++ src/Product.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Product.css'
+
+const Product = ({id,title,rating,price,image}) => {
+    return (
+        <div className='product'>
+            <div className='product__info'>
+                <p>{title}</p>
+                <p className='product__price'>
+                    <small>Sh.</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className='product__rating'>
+                    {
+                    Array(rating)
+                    .fill()  
+                    .map((_) =>(
+                        <p>🌟</p>  
+                    ))
+                    }
+                </div>
+            </div>
+           
+            <img  src={image} alt=''/>
+            <button>Add to basket</button>
+        </div>
+    )
+}
+
+export default Product
