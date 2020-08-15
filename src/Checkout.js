f00387d6dad45faa3d3583779722de71a64d9d72
barackmaund1 <pmaundu49@gmail.co>
2020-08-15 13:27:58 +0300

    install react-currency-format to build subtotal

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,43 +1,54 @@
 import React from 'react'
 import './Checkout.css'
 import { useStateValue } from './StateProvider';
-import CheckoutProduct from './CheckoutProduct'
+import CheckoutProduct from './CheckoutProduct';
+import Subtotal from './Subtotal';
 
 const Checkout = () => {
     const [{basket}]= useStateValue();
 
     return (
-        <div className='checkout__ad'>
-            <img className='checkout__ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._C8423492668_.jpg'/>
-             {
-                 basket?.length === 0 ?(
-                     <div>
-                       <h2>Your Shopping Basket is empty</h2>
-                     <p>
-                     You have no items in your basket.To buy one or more items,click
-                     "Add to basket " next to the item.
-                     </p>
-                       </div>
-                 ):(
-                     <div>
-                      <h2 className='checkout__title'>Your shopping  basket 🧺 </h2>
-                     {/* List out all of the Checkout Products*/}
-                      {basket?.map(item =>(
+        <div className='checkout'>
+            <div className='checkout__left'>
+                <img className='checkout__ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._C8423492668_.jpg'/>
+                {
+                    basket?.length === 0 ?(
+                        <div>
+                        <h2>Your Shopping Basket is empty</h2>
+                        <p>
+                        You have no items in your basket.To buy one or more items,click
+                        "Add to basket " next to the item.
+                        </p>
+                        </div>
+                    ):(
+                        <div>
+                        <h2 className='checkout__title'>Your shopping  basket 🧺 </h2>
+                        {/* List out all of the Checkout Products*/}
+                        {basket?.map(item =>(
+                            
+                            
+                            <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                            />
+                        ))}
                         
-                        
-                        <CheckoutProduct
-                        id={item.id}
-                        title={item.title}
-                        image={item.image}
-                        price={item.price}
-                        rating={item.rating}
-                        />
-                      ))}
-                     
-                     </div>
-                 )}
+                        </div>
+                    )}
+                    
             </div>
+           {basket.length >0 && (
+              <div className='checkout__right'>
+                 <Subtotal/>
+              </div>
+          )}
+        </div>
+
+            
     )
-}
+};
 
 export default Checkout
---
+++ src/Subtotal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Subtotal.css'
+
+const Subtotal = () => {
+    return (
+        <div className='subtoatal'>
+          {/* Price */}
+            <button>Proceed to checkout</button>
+        </div>
+    )
+}
+
+export default Subtotal
